2dab887ad699c0e592ea3a65bc66f2bb9a184158
Peschernuy <peschernuy@gmail.com>
2022-01-15 18:41:16 +0200

    #8 lesson Categories and SortPopup sorting

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import React from 'react';
-import axios from 'axios';
-import {  useDispatch } from 'react-redux';
-
-import { Header} from './components';
-import {Home, Cart} from './pages';
+import { Header } from './components';
+import { Home, Cart } from './pages';
 import { Route, Routes } from 'react-router-dom';
-import { setPizzas } from './redux/actions/pizzas';
-
-import { setCategory } from './redux/actions/filters';
-
-console.log(setCategory);
 
 
-function App () {
-
-  const dispatch = useDispatch();
-  
-  React.useEffect(() => {
-      axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-        dispatch(setPizzas(data));
-      });
-  }, []);
+function App() {
 
 
-return (
-  <div className="wrapper">
-    <Header />
-    <div className="content">
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/cart" element={<Cart />} exact />
-      </Routes>
+
+  return (
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/cart" element={<Cart />} exact />
+        </Routes>
+      </div>
     </div>
-  </div>
-);
-
-};
+  );
+}
 
 export default App;
-
